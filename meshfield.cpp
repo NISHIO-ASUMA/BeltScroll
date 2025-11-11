1039c584cf171492bba3d9d684fffd0e85572937
矢吹 幸翔 <241323@st.yoshida-g.ac.jp>
2025-11-11 15:28:05 +0900

    テクスチャ変更＋床を新しいやつに変えた

--- meshfield.cpp
+++ meshfield.cpp
@@ -77,7 +77,7 @@ void CMeshField::SetTexture(void)
 	CTexture* pTexture = CManager::GetTexture();
 
 	// Š„‚è“–‚Ä
-	m_nTexIdx = pTexture->Register("data/TEXTURE/Road_cha.jpg");
+	m_nTexIdx = pTexture->Register("data/TEXTURE/Road_cha003.png");
 }
 //============================================
 // ‰Šú‰»ˆ—
