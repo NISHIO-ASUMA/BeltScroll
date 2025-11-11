2d0315cc6399124b9bac6f186462536412c37ba7
NISHIO-ASUMA <241315@st.yoshida-g.ac.jp>
2025-11-11 15:52:39 +0900

    json変更

--- blockmanager.cpp
+++ blockmanager.cpp
@@ -48,7 +48,7 @@ HRESULT CBlockManager::Init(void)
 	m_block.clear();
 
 	// jsonŠJ‚­
-	LoadJsonScript("data/JSON/Stage.json");
+	LoadJsonScript("data/JSON/testgame.json");
 
 	// ‰Šú‰»Œ‹‰Ê‚ğ•Ô‚·
 	return S_OK;
