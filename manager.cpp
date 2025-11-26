33ef5ebcada571f8c435991c407008288279fddd
NISHIO-ASUMA <241315@st.yoshida-g.ac.jp>
2025-11-26 11:06:40 +0900

    ステージ配置,モデル変更,リザルトいじってます

--- manager.cpp
+++ manager.cpp
@@ -134,7 +134,7 @@ HRESULT CManager::Init(HINSTANCE hInstance, HWND hWnd, BOOL bWindow)
 
 #ifdef _DEBUG
 	// タイトルシーンをセット
-	m_pFade->SetFade(std::make_unique<CTitle>());
+	m_pFade->SetFade(std::make_unique<CGame>());
 #else
 	// シーンセット
 	m_pFade->SetFade(std::make_unique<CTitle>());
